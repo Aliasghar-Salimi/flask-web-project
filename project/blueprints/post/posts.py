90fb3fce79a174247f32ebbddd951bc344360780
Aliasghar-salimi <aliasghar.salimi.05@gmail.com>
2024-07-23 19:05:52 +0330

    add_tag view added, fetch_posts view compleated, create_post view compleated

--- project/blueprints/post/posts.py
+++ project/blueprints/post/posts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect, flash
 import connection
 import json
 
@@ -12,14 +12,44 @@ def create_post():
 
     if request.method == "POST":
         desc = request.form.get("desc")
+        tags = request.form.getlist("tags")
+        print(tags)
 
-        insertion_query = f"INSERT INTO posts (description) VALUES (\"{desc}\")"
-        cursor.execute(insertion_query)
+        desc_insertion_query = f"INSERT INTO posts (description) VALUES (\"{desc}\")"
+        cursor.execute(desc_insertion_query)
         cnx.commit()
 
-        return json.dumps({"post":"created"})
+        tag_ids = []
+        for tag in tags:
+            fetch_tags_quary = f"SELECT id FROM the_tags WHERE name=\"{tag}\""
+            cursor.execute(fetch_tags_quary)
+            the_tag = cursor.fetchone()
+            tag_ids.append(the_tag)
+        print(tag_ids)
+
+        fetch_post_ids = f"SELECT id FROM posts"
+        cursor.execute(fetch_post_ids)
+        post_ids = cursor.fetchall()
+        post_id = post_ids[len(post_ids)-1:][0][0]
+        print(post_id)
+
+        for tag_id in tag_ids:
+            tag_insertion_query = f"""INSERT INTO post_tag (tag_id, post_id)
+                                    VALUES (\"{tag_id[0]}\", \"{post_id}\")"""
+            cursor.execute(tag_insertion_query)
+            cnx.commit()
+
+        cursor.close()
+        cnx.close()
+        return redirect("/posts/")
     else:
-        return render_template('create_post.html')
+        fetch_tags_quary = "SELECT id, name FROM the_tags"
+        cursor.execute(fetch_tags_quary)
+        tags = cursor.fetchall()
+
+        cursor.close()
+        cnx.close()
+        return render_template('create_post.html', tags=tags)
     
 
 @posts_blueprint.route("/posts/", methods=["GET"])
@@ -29,43 +59,68 @@ def fetch_posts():
     cursor = cnx.cursor(buffered=True)
 
     # Fetching the post id's and descriptions to display in the page
-    fetch_query = "SELECT id, description FROM posts"
-    cursor.execute(fetch_query)
+    fetch_post_query = "SELECT id, description FROM posts"
+    cursor.execute(fetch_post_query)
     posts = cursor.fetchall()
 
-    # Fetching the categroy id's names to mathe the names with the id's in the post tag table
-    fetch_tagNames = "SELECT id, name from categories"
-    cursor.execute(fetch_tagNames)
+    # Fetching the tag id's names to mathe the names with the id's in the post tag table
+    fetch_tags = "SELECT id, name from the_tags"
+    cursor.execute(fetch_tags)
     tags = cursor.fetchall()
 
-    # saving the category id's and names in a dictionary called categories
-    categories = {}
+    # saving the tag id's and names in a dictionary called tags
+    tag_dict = {}
     for tag in tags:
-        categories.update({tag[0]:tag[1]})
-    print("category id and names => "+str(categories))
+        tag_dict.update({tag[0]:tag[1]})
 
     # extract post id's from the fetched posts to ... 
     post_ids = []
     for post in posts:
         post_ids.append(post[0])
-    
-    print("post ids => "+str(post_ids)+"\n")
-    
-    tags = []
+
     post_tags = {}
 
     for post_id in post_ids:
         # Fetching the tags related to each post
-        fetch_categories = f"SELECT cat_id from post_tag WHERE post_id={post_id}"
-        cursor.execute(fetch_categories)
-        the_tags = cursor.fetchall()
-        post_tags.update({post_id:the_tags})
+        fetch_tag_ids = f"SELECT tag_id from post_tag WHERE post_id={post_id}"
+        cursor.execute(fetch_tag_ids)
+        tag_ids = cursor.fetchall()
+        post_tags.update({post_id:tag_ids})
 
-    print(post_tags.keys())
-    for post_id in post_tags:
-        for ids in post_tags[post_id]:
-            for category in categories:
-                if category == ids[0]:
-                    print(categories[category])
+    cursor.close()
+    cnx.close()
+    return render_template('posts.html', posts=posts, post_tags=post_tags, tag_dict=tag_dict)
 
-    return render_template('posts.html', posts=posts, post_tags=post_tags, categories=categories)
+@posts_blueprint.route('/delete-post/', methods=["POST"])
+def delete_post():
+    cnx = connection.connect()
+    cursor = cnx.cursor(buffered=True)
+
+
+@posts_blueprint.route('/add-tag/', methods=['POST', 'GET'])
+def add_tag():
+
+    if request.method == 'POST':
+        cnx = connection.connect()
+        cursor = cnx.cursor(buffered=True)
+        
+        tag_names = request.form.get("tags")
+        tag_names = tag_names.split(' ')
+
+        print(tag_names)
+
+        for tag in tag_names: 
+            print(tag)
+            tag_insertion_query = f"""INSERT INTO the_tags (name) VALUES (\"{tag}\")"""
+            cursor.execute(tag_insertion_query)
+            cnx.commit()
+
+        if cursor.rowcount > 0:
+            flash("tags successfully added", category='success')
+            return redirect('/create-post/')
+        else:
+            flash("tags were not added", category='error')
+            return redirect("add_tag")
+    else:
+        return render_template("add_tag.html")
+    
